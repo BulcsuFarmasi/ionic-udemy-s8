35e600573c17897bc3930429764dbaf954f7efb1
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-10-26 21:03:37 +0200

    adding google maps

--- src/pages/add-place/add-place.ts
+++ src/pages/add-place/add-place.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { ModalController} from 'ionic-angular';
+
+import { SetLocationPage } from '../set-location/set-location';
+
 @Component({
   selector: 'page-add-place',
   templateUrl: 'add-place.html',
 })
 export class AddPlacePage {
+    
+    constructor (private modalController:ModalController) {}
+
+    onOpenMap () {
+        const modal = this.modalController.create(SetLocationPage);
+        modal.present();
+    }
+
     onSubmit(form: NgForm) {
       console.log(form.value);
     }
