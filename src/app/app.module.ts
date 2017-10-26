78df662c1ae7a78e26c23edec59e4132751cbb3a
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-10-26 11:04:07 +0200

    creating pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
+import { AddPlacePage } from '../pages/add-place/add-place';
 import { HomePage } from '../pages/home/home';
+import { PlacePage } from '../pages/place/place';
+import { SetLocationPage } from '../pages/set-location/set-location';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    AddPlacePage,
+    HomePage,
+    PlacePage,
+    SetLocationPage  
   ],
   imports: [
     BrowserModule,
@@ -19,7 +25,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    AddPlacePage,
+    HomePage,
+    PlacePage,
+    SetLocationPage
   ],
   providers: [
     StatusBar,
